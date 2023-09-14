5872023aa5a36c59ed3d4dc87781c4a926f14100
michelbsilva <michelbsilva94@gmail.com>
2023-09-13 23:02:49 -0300

    Ajuste menu responsivo e logo

---
+++ resources/views/components/application-logo-login.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('img/logoburaco.jpeg')}}" alt="" srcset="" width="180">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -62,7 +62,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Início') }}
             </x-responsive-nav-link>
         </div>
 
@@ -75,9 +75,32 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Perfil') }}
                 </x-responsive-nav-link>
-
+                <x-responsive-nav-link :href="route('services.peopleService')">
+                    {{ __('Cadastro de Pessoas') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('contract.index')">
+                    {{ __('Contratos') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('monthly.index')">
+                    {{ __('Geração de Mensalidades') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('monthly.tuition')">
+                    {{ __('Mensalidades') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('user.index')">
+                    {{ __('Gestão de Usuários') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('services.securityService')">
+                    {{ __('Gestão de Segurança') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('services.structureService')">
+                    {{ __('Estrutura') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('services.domainService')">
+                    {{ __('Domínios') }}
+                </x-responsive-nav-link>
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
@@ -85,7 +108,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Sair') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
