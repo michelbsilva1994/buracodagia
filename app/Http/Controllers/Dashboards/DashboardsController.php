7e31dd98fa1535f9a2271e9fcb78313dde46cddd
michelbsilva <michelbsilva94@gmail.com>
2024-03-26 22:52:46 -0300

    Tela de Dashboard

--- app/Http/Controllers/Dashboards/DashboardsController.php
+++ app/Http/Controllers/Dashboards/DashboardsController.php
@@ -11,24 +11,29 @@ use Illuminate\Http\Request;
 class DashboardsController extends Controller
 {
 
-    public function __construct(LowerMonthlyFee $LowerMonthlyFee)
+    public function __construct(LowerMonthlyFee $LowerMonthlyFee, MonthlyPayment $MonthlyPayment)
     {
         $this->LowerMonthlyFee = $LowerMonthlyFee;
+        $this->MonthlyPayment = $MonthlyPayment;
     }
 
     public function dashboard(){
 
-        $dinheiro = $this->LowerMonthlyFee->where('id_type_payment', '=', 'D')->sum('amount_paid');
+        $total_receivable = $this->MonthlyPayment->where('id_monthly_status', '<>', 'C')->sum('total_payable');
+        $total_paid = $this->MonthlyPayment->where('id_monthly_status', '<>', 'C')->sum('amount_paid');
+        $total_received = $this->MonthlyPayment->where('id_monthly_status', '<>', 'C')->sum('balance_value');
+
+        $money = $this->LowerMonthlyFee->where('id_type_payment', '=', 'D')->sum('amount_paid');
         $pix = $this->LowerMonthlyFee->where('id_type_payment', '=', 'P')->sum('amount_paid');
-        $cartao_debito = $this->LowerMonthlyFee->where('id_type_payment', '=', 'CD')->sum('amount_paid');
-        $cartao_credito = $this->LowerMonthlyFee->where('id_type_payment', '=', 'CC')->sum('amount_paid');
+        $debit_card = $this->LowerMonthlyFee->where('id_type_payment', '=', 'CD')->sum('amount_paid');
+        $credit_card = $this->LowerMonthlyFee->where('id_type_payment', '=', 'CC')->sum('amount_paid');
 
         $dashboard = new Dashboard;
 
         $dashboard->labels(['Pix', 'Dinheiro', 'Cartão Débito','Cartão Crédito' ]);
 
-        $dashboard->dataset('Valores Recebidos', 'doughnut', [$pix,$dinheiro,$cartao_debito,$cartao_credito])->backgroundColor(['#2980b9','#2c3e50','#f1c40f','#8e44ad']);
+        $dashboard->dataset('Valores Recebidos', 'bar', [$pix,$money,$debit_card,$credit_card])->backgroundColor(['#227093','#218c74','#84817a','#2c2c54']);
 
-        return view('dashboards.dashboardTeste', compact('dashboard'));
+        return view('dashboards.dashboardTeste', compact(['dashboard', 'pix', 'money','debit_card','credit_card','total_receivable', 'total_paid', 'total_received']));
     }
 }
--- resources/views/dashboards/dashboardTeste.blade.php
+++ resources/views/dashboards/dashboardTeste.blade.php
@@ -1,7 +1,26 @@
 @extends('layout.app')
 @section('content')
-    <div class="col-6">
-        {!! $dashboard->container() !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                {!! $dashboard->container() !!}
+            </div>
+            <div class="col-6 text-secondary text-center">
+                <div>
+                    <h3 class="mt-5">Valores Recebidos</h3>
+                    <h5>PIX: R$ {{number_format($pix, 2, ',', '.')}}</h5>
+                    <h5>Dinheiro: R$ {{number_format($money, 2, ',', '.')}}</h5>
+                    <h5>Cartão de Débito: R$ {{number_format($debit_card, 2, ',', '.')}}</h5>
+                    <h5>Cartão de Crédito: R$ {{number_format($credit_card, 2, ',', '.')}}</h5>
+                </div>
+                <div class="mt-5">
+                    <h3>Totais</h3>
+                    <h5>Valor Total: R$ {{number_format($total_receivable, 2, ',', '.')}}</h5>
+                    <h5>Total Recebido: R$ {{number_format($total_paid, 2, ',', '.')}}</h5>
+                    <h5>Cartão à Receber: R$ {{number_format($total_received, 2, ',', '.')}}</h5>
+                </div>
+            </div>
+            {!! $dashboard->script() !!}
+        </div>
     </div>
-    {!! $dashboard->script() !!}
 @endsection
