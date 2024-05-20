c2b4b50ba86d94e4a053e2c283d5b0d62656f221
michelbsilva <michelbsilva94@gmail.com>
2024-05-19 21:51:08 -0300

    Ajuste cabeçalho

--- resources/views/monthlyPayment/homeMonthlyPayment.blade.php
+++ resources/views/monthlyPayment/homeMonthlyPayment.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="col-12">
-            <h1 class="my-4 text-secondary text-center">Segurança</h1>
+            <h1 class="my-4 text-secondary text-center">Geração de mensalidades</h1>
         </div>
         <div class="d-grid gap-2 col-sm-12 col-md-8 my-4 mx-auto">
             <a class="btn btn-lg btn-success" type="button" href="{{route('monthly.index')}}">Gerar Mensalidades</a>
