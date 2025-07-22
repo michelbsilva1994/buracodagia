e8e9cc776f6d28c029c965ebe8f7c8b87c977e33
michelbsilva <michelbsilva94@gmail.com>
2025-07-21 21:40:00 -0300

    AJuste de url

--- resources/views/reports/lowerTuition.blade.php
+++ resources/views/reports/lowerTuition.blade.php
@@ -55,7 +55,9 @@
 $(document).ready(function() {
     $('#pavement').on('change', function() {
         let pavementId = $(this).val();
-        let url = 'public/reports/stores/by-pavement/' + (pavementId ? pavementId : '');
+        //let url = 'public/reports/stores/by-pavement/' + (pavementId ? pavementId : '');
+        let url = "{{route('reports.storesByPavement', ['pavement_id' => ':id'])}}";
+        url = url.replace(':id', pavementId);
 
         $.get(url, function(data) {
             let $storeSelect = $('#store');
